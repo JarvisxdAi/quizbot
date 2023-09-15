acd67009f2d4de437e25c4a09ed2c4cf6b232e54
JarvisxdAi <123392833+JarvisxdAi@users.noreply.github.com>
2023-09-15 14:59:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,128 +1,42 @@
-"""
-Simple Quiz Bot for Telegram
-"""
+import telebot
+import requests
+import time
 
-from datetime import datetime
-import logging
-from logging import debug, info
-from pathlib import Path
-import random
-import telegram
-from telegram.ext import Updater, CommandHandler, MessageHandler, PicklePersistence
-import yaml  # pyyaml
+bot = telebot.TeleBot("5963775988:AAHEIZ6vo2hCnAVMNrQGBOdLo7jUs-AGS54")  # Replace with your bot token
 
-
-logging.basicConfig(
-    filename='conversations.log',
-    format='%(asctime)s %(levelname)-7s %(name)s %(message)s')
-logging.getLogger().setLevel('DEBUG')
-
-TOKEN_FILE = 'token.txt'
-TOKEN = Path(TOKEN_FILE).read_text().strip()
-AUTHORIZED_USERS = ['makukha']
-
-DURATION = 5
+# /start command for your bot
+@bot.message_handler(commands=['start'])
+def handle_start(message):
+    start_message = '''
+<b>Hey Folks,
+Welcome to Tocsi Server Key Generator
+Tab Here For Genrate 1hrs Key
+      /getkey 
+It's Work In All App
+Join Our Channel @Tocsiserver </b>
+    '''
+    bot.reply_to(message, start_message, parse_mode='HTML')
 
 
-class Question:
-
-    def __init__(self, qid, question, answers):
-        self.qid = qid
-        self.text = question
-        self.answers = {}
-        self.correct = None
-        for a in answers:
-            aid = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'[len(self.answers)]
-            if isinstance(a, str):
-                self.answers[aid] = a
-            elif isinstance(a, dict) and len(a) == 1 and \
-                    'correct' in a and self.correct is None:
-                self.answers[aid] = a['correct']
-                self.correct = aid
-            else:
-                raise ValueError(
-                    f'Incorrect answers in question {qid}: {answers}')
-
-QUESTIONS = {q['id']: Question(q['id'], q['q'], q['a'])
-    for q in yaml.load(Path('questions.yaml').read_text())}
+@bot.message_handler(commands=['getkey'])
+def handle_start(message):
+    res = requests.get('https://tocsiiserver.xyz/Key/redirect.php')
+    g = (res.text)
+    t = '''
+<b> We Genrate Key For 1 Hrs❤️
+your Key Is 👇🏻👇🏻👇🏻</b>
+    '''
+    r = requests.get('https://api.github.com/events', stream = True)
+    bot.reply_to(message, t+g[2651:2668], parse_mode='HTML')
+    
 
 
-def start(update, context):
-    """Command handler for command /start"""
+# A text message handler
+@bot.message_handler(func=lambda message: True)
+def handle_text(message):
+    reply_text = message.text
+    bot.reply_to(message, reply_text)
 
-    msg = update.message
-    user = msg.from_user
-    debug(f'Quiz bot entered by user: {user.id} @{user.username} "{user.first_name} {user.last_name}"')
+# Start the bot
 
-    if AUTHORIZED_USERS and user.username not in AUTHORIZED_USERS:
-        return
-
-    if 'username' not in context.user_data:
-        context.user_data['username'] = user.username
-
-    msg.bot.send_message(msg.chat_id,
-        text=f'Давайте начнем тест. У вас будет {DURATION} минут на {len(QUESTIONS)} вопросов. Готовы?',
-        reply_markup=telegram.ReplyKeyboardMarkup([['Начать тест']]))
-
-
-def common_message(update, context):
-    """General response handler"""
-
-    msg = update.message
-    user = msg.from_user
-    debug(f'Message received from {user.id} @{user.username}: {msg.text}')
-
-    if AUTHORIZED_USERS and user.username not in AUTHORIZED_USERS:
-         return
-
-    if 'quiz' not in context.user_data:
-
-        info(f'Quiz started by {user.id} @{user.username}')
-
-        context.user_data['quiz'] = {}
-        context.user_data['quiz']['answers'] = {}
-        starttime = datetime.now()
-        context.user_data['quiz']['starttime'] = starttime
-
-        msg.bot.send_message(msg.chat_id,
-            text=f'Тест начат в {starttime}',
-            reply_markup=telegram.ReplyKeyboardRemove())
-
-    else:
-        # save response
-        context.user_data['quiz']['answers'][context.user_data['quiz']['current_qid']] = msg.text
-
-    # ask the question
-
-    questions_left = set(QUESTIONS) - set(context.user_data['quiz']['answers'])
-
-    if len(questions_left) > 0:
-
-        question = QUESTIONS[random.sample(questions_left, 1)[0]]
-
-        msg.bot.send_message(msg.chat_id,
-            text=f'{question.text}\n' + \
-                '\n'.join(f'{aid}. {text}' for aid, text in sorted(question.answers.items())),
-            reply_markup=telegram.ReplyKeyboardMarkup([[aid for aid in sorted(question.answers)]]))
-
-        context.user_data['quiz']['current_qid'] = question.qid
-
-    else:
-        msg.bot.send_message(msg.chat_id,
-            text=f'Тест пройден!',
-            reply_markup=telegram.ReplyKeyboardRemove())
-        context.user_data['quiz']['current_qid'] = None
-
-
-def main():
-    storage = PicklePersistence(filename='data.pickle')
-    updater = Updater(token=TOKEN, persistence=storage, use_context=True)
-    dp = updater.dispatcher
-    dp.add_handler(CommandHandler('start', start))
-    dp.add_handler(MessageHandler(None, common_message))
-    updater.start_polling()
-    updater.idle()
-
-
-if __name__ == '__main__':
-    main()
+bot.polling()
